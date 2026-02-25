9335291f9c6a1dd8219660c4a529c487b965c6f7
Thorng RattanakChakaval <ChakavalTR@yahoo.com>
2026-02-25 15:04:19 +0700

    first commit app

---
+++ lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:app_list_to_do/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomeScreen());
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:app_list_to_do/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
